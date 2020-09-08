3f83ffa8a7c1898967b17f9e0835f5120d2fac4f
adqn <zeroeiz@gmail.com>
2020-09-08 06:29:18 -0700

    misc

--- irc_console.py
+++ irc_console.py
@@ -64,43 +64,49 @@ def init_env(shell, f):
         print(e)
         print("Could not connect to a bot. Type \"host connect\" to try again.")
 
-while True:
-    #configfile = 
-    currentfile = check_command_file(commandfile)
+if __name__ == '__main__':
+    host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    shell = IRCShell(host)
+    init_env(shell)
 
-    if shell.action == "default":
-        if len(shell.env['channels']) != 0:
-            shell.curr_state = " ".join(shell.env['channels']) + "\n"
+    while True:
+        shell.curr_com = None
 
-    try:
-        inp = str(input("%s@%s> " % (shellname, shell.curr_chan)))
-        
-        if inp[0] == "/":   
-            com = inp.split(" ")[0]
-            msg = " ".join(inp.split(" ")[1:])
+        try:
+            if shell.curr_chan != "":
+                indicator = "@"
+            else:
+                indicator = ""
+
+            inp = str(input("%s%s%s> " % (shellname, indicator, shell.curr_chan)))
             
-            if com == "/c":
-                if shell.curr_chan != None:
-                        shell.curr_com = "\n".join([shell.curr_chan, msg])
-                else:
-                    pass
+            if len(inp) > 0:
+                if inp[0] == "/":   
+                    com = inp.split(" ")[0]
+                    msg = " ".join(inp.split(" ")[1:])
+                    
+                    if com == "/c":
+                        if shell.curr_chan != None:
+                                shell.curr_com = "\n".join([shell.curr_chan, msg])
+                        else:
+                            pass
 
-            if com == "/next":
-                if shell.curr_chan_id == len(shell.env['channels']):
-                    shell.curr_chan_id = 1
-                else:
-                    shell.curr_chan_id += 1
-                shell.curr_chan = shell.env['channels'][shell.curr_chan_id - 1]         
+                    if com == "/next":
+                        if shell.curr_chan_id == len(shell.env['channels']):
+                            shell.curr_chan_id = 1
+                        else:
+                            shell.curr_chan_id += 1
+                        shell.curr_chan = shell.env['channels'][shell.curr_chan_id - 1]         
 
-            if com == "/all":
-                shell.curr_com = "\n".join([" ".join(shell.env['channels']), msg])
-            
+                    if com == "/all":
+                        shell.curr_com = "\n".join([" ".join(shell.env['channels']), msg])
+                    
 
-        if shell.curr_com != None:
-            with open(commandfile, "w") as c:  
-                c.write(shell.curr_com)
-            shell.curr_com = shell.curr_state
+                if shell.curr_com != None:
+                    with open(commandfile, "w") as c:  
+                        c.write(shell.curr_com)
+                    shell.curr_com = shell.curr_state
 
-    except KeyboardInterrupt:
-        print("\nCTRL+C was used; shell interrupted.")
-        break
+            except KeyboardInterrupt:
+                print("\nCTRL+C was used; shell interrupted.")
+                break
