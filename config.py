d6981dab2a208de90053043bc841d974d03f5bfc
adqn <zeroeiz@gmail.com>
2020-09-08 18:05:45 -0700

    misc

---
+++ config.py
@@ -0,0 +1,18 @@
+server = {
+    'server': port,
+}
+
+channels = [
+    '#channelgoeshere',
+]
+
+# Name: password (None if not applicable)
+nick = {
+    'My_bot': None'
+}
+
+scripts = [
+
+]
+
+shellname = 'CAAbot'
