6b9e48db5265d423b6c8bf9acda16f3e1a97f99d
adqn <zeroeiz@gmail.com>
2020-09-10 16:06:09 -0700

    misc

--- config.py
+++ config.py
@@ -1,18 +1,30 @@
-server = {
-    'server': port,
+########################
+##### Bot settings #####
+######################## 
+
+servers = {
+    'irc.rizon.net': 6667,
 }
 
 channels = [
-    '#channelgoeshere',
+    '#agagag',
 ]
 
-# Name: password (None if not applicable)
-nick = {
-    'My_bot': None'
-}
+# password is None if not applicable #
+
+botnick = 'sukub'
+password = None
 
 scripts = [
-
+    'logging2',
+    'filtering',
+    'get_qts',
 ]
 
+##########################
+##### Shell settings #####
+##########################
+
 shellname = 'CAAbot'
+bot_address = 'bot.ip.here'
+bot_port = 8181
