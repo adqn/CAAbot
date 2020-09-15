be7dc0a8df842ffc6dd439a497f64c65e4ecb15e
adqn <zeroeiz@gmail.com>
2020-09-15 12:28:06 -0700

    misc

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ use cpython::{Python, PyDict, PyResult};
 
 fn main() { 
     let gil = Python::acquire_gil();
-    hello(gil.python()).unwrap();
+    get_sys(gil.python()).unwrap();
 }
 
 pub fn get_py_path(py: Python) -> PyResult<()> {
-     let locals = PyDict::new(py);
+    let locals = PyDict::new(py);
     locals.set_item(py, "sys", py.import("sys")?)?; 
     let path: String = py.eval("str(sys.path)", None, Some(&locals))?.extract(py)?;
     println!("Python path: {}", path);
     Ok(())
 }
 
-pub fn hello(py: Python) -> PyResult<()> {
+pub fn get_sys(py: Python) -> PyResult<()> {
     let sys = py.import("sys")?;
     let version: String = sys.get(py, "version")?.extract(py)?;
 
