cc615f674b9ba4372d633f0aa0e94d41a4b19ce6
adqn <zeroeiz@gmail.com>
2020-09-20 18:02:32 -0700

    logging stuff

--- logging2.py
+++ logging2.py
@@ -23,3 +23,19 @@ class Logger:
             'bufsize': self.bufsize,
             'current channels': self.bot.channels.copy()
         }
+
+    def get_message(self, message):
+        pass
+
+    def log_stuff(self):
+        if any(self.message_buffer):
+            if len(self.message_buffer) >= self.bufsize:
+                self.message_buffer = self.message_buffer[1:]
+
+        if self.bot.message_queue[-1].find("PRIVMSG #")  != -1 or self.bot.message_queue[-1].find("MODE #") != -1:
+            message = self.bot.message_queue[-1]
+            self.message_buffer.append(message)
+
+            if self.logging:
+                with open("logtest.txt", 'a') as log:
+                    log.write(message)
