af83eccf425027bac0e22507aeab0647076e85c2
adqn <zeroeiz@gmail.com>
2020-09-23 12:57:43 -0700

    added safebooru random image fetcher

--- get_qts.py
+++ get_qts.py
@@ -22,3 +22,23 @@ class Cutie:
     def get_env(self):
         return {}
 
+    def get_command(self):
+        msg = None
+
+        if self.curr_msg.find("PRIVMSG #") != -1 and self.curr_msg.find(":.qt") != -1:
+            if self.curr_msg.find("PRIVMSG #") != -1 and self.curr_msg.find("SLAPP_") != -1:
+                return
+            
+            calling_channel = self.curr_msg[self.curr_msg.find("PRIVMSG #"):].split(" ")[1]
+            com = self.curr_msg[self.curr_msg.find(":.qt"):].replace(":", "").replace("\r\n", "").split(" ")
+
+            if com[0] == ".qt":
+                if len(com) == 1:  # get random qt
+                    msg = self.get_qt_url()
+
+                else: # get qt by tags
+                    com[-1] = com[-1].replace("\r\n", "")
+                    msg = self.get_qt_url(com[1:])
+
+            if msg:
+                self.bot.send_msg(calling_channel, msg)
