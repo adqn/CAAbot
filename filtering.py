e79cc4ec9617b033f8ed1e5aa6c30e4b2150240f
adqn <zeroeiz@gmail.com>
2020-09-25 18:12:31 -0700

    filtering stuff

--- filtering.py
+++ filtering.py
@@ -23,6 +23,30 @@ class Filter:
     def get_env(self):
         return {}
 
+    def auto_reply(self):
+        bufsize = 7 # Amount of messages to get per call
+
+        try:
+            temp_buffer = self.bot.message_queue[-bufsize:]
+
+            for m in temp_buffer:         
+                for string in filter_list:       
+                    if m.find(string) != -1:        
+                        filter_list[string][1] += 1
+                    else:
+                        filter_list[string][1] = 0
+                
+            for string in filter_list:
+                if filter_list[string][1] == filter_list[string][2]:
+                    channel = filter_list[string][0]
+                    self.bot.send_msg(channel, filter_list[string][3])
+
+                filter_list[string][1] = 0
+            
+        except Exception as e:
+            print("what the", e)
+    
+
 
     def main_thread(self):
         while self.running:
