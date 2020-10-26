134d7940e548aa70ec0bd4130eeab676933f5ec2
adqn <57331120+adqn@users.noreply.github.com>
2020-10-26 14:24:22 -0700

    misc

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,155 @@
-// Hopefully some functions and such to take over a few python things.
+use std::io::prelude::*;
+use std::io::{Read, Write};
+use std::io::BufReader;
+use std::net::{TcpStream};
+use std::str::from_utf8;
 
-use cpython::{Python, PyDict, PyResult};
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
-fn main() { 
-    let gil = Python::acquire_gil();
-    get_sys(gil.python()).unwrap();
+use regex::Regex;
+
+// Currently running threads
+struct Threads {
+
 }
 
-pub fn get_py_path(py: Python) -> PyResult<()> {
-    let locals = PyDict::new(py);
-    locals.set_item(py, "sys", py.import("sys")?)?; 
-    let path: String = py.eval("str(sys.path)", None, Some(&locals))?.extract(py)?;
-    println!("Python path: {}", path);
-    Ok(())
+// Currently loaded/unloaded modules
+struct Modules {
+
 }
 
-pub fn get_sys(py: Python) -> PyResult<()> {
-    let sys = py.import("sys")?;
-    let version: String = sys.get(py, "version")?.extract(py)?;
+// Stores and handles bot environment
+struct Bot {
+    running: bool,
+    channels: Vec<String>,
+    current_message: Option<String>,
+    //modules: Some(Vec<M>),
+    //threads: Some(Vec<T>),
+}
 
-    let locals = PyDict::new(py);
-    locals.set_item(py, "os", py.import("os")?)?;
-    let user: String = py.eval("os.getenv('USER') or os.getenv('USERNAME')", None, Some(&locals))?.extract(py)?;
-    println!("Hello {}, this is Python {}", user, version);
-    Ok(())
+impl Bot {
+    fn new(channels: Vec<String>) -> Bot {
+        Bot{
+            running: true,
+            channels,
+            current_message: None,
+            //modules: vec![None],
+            //threads: None,
+        }
+    }
+}
+
+
+fn connect(server: String, port: Option<String>) -> Option<TcpStream> {
+    let mut conn: TcpStream;
+    let mut host = server.to_owned();
+
+    let mut port = match port {
+        Some(port) => port,
+        None => String::from("6667"),
+    };
+   
+    host.push_str(":");
+    host.push_str(&port);
+
+    println!("Connecting to host...");
+
+    match TcpStream::connect(host) {
+        Ok(mut stream) => {
+            Some(stream)
+        },
+        Err(e) => {
+            println!("error: {}", e);
+            None
+        }
+    }
+}
+
+fn get_resp(mut stream: &TcpStream) -> Option<String> {
+    let mut data = [0u8; 2048];
+
+    match stream.read(&mut data) {
+        Ok(_) => {
+            let text = from_utf8(&data).unwrap();
+            Some(String::from(text))
+        },
+        Err(e) => {
+            println!("error: {}", e);
+            None
+        }
+    }
+}
+
+fn send(mut stream: &TcpStream, msg: &str) {
+    let mut msg = String::from(msg).to_owned();
+    msg.push_str("\n");
+    stream.write(msg.as_bytes()).unwrap();
+}
+
+fn server_thread(stream: &TcpStream, sender: &Sender<String>) {
+    let ping_re = Regex::new(r"PING").unwrap();
+    let pong_re = Regex::new(r"PONG \d+").unwrap();
+    let response: String;
+ 
+    response = match get_resp(&stream){
+        None => String::from("Couldn't receive message from server"),
+        Some(resp) => {
+            println!("{}", resp);
+           
+            // Reply to server ping
+            if ping_re.is_match(&resp[..]) {
+                send(&stream, "PONG :pingis\n");
+            } 
+
+            // Reply to initial pong request> to complete connection
+            if pong_re.is_match(&resp[..]) {
+                for cap in pong_re.captures_iter(&resp[..]) {
+                    send(&stream, &cap[0]);
+                }
+            }
+
+            resp
+        }
+    };
+   
+    sender.send(response).unwrap();
+}
+
+fn main() {
+    let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+
+    let mut bot = Bot::new(vec![String::from("#afafaf")]);
+    let ident = "USER owo test tet test :test";    
+    let nick = "NICK CAA_rust_test";
+
+    let hostip = String::from("irc.rizon.net");
+    let port = Some(String::from("6667"));
+
+    match connect(hostip, port) {
+       Some(s) => {
+            println!("Successfully connected");
+ 
+            // Send our identity and set nickname
+            send(&s, ident);
+            send(&s, nick);           
+           
+            // Spawn thread for persistent server connection
+            let sthread = thread::spawn(move || {
+                if bot.running == true {
+                    loop {
+                        server_thread(&s, &tx);
+                        bot.current_message = Some(rx.recv().unwrap());
+                        //println!("from tx: {}", bot.current_message.unwrap());
+                    }
+                }
+            });
+            // Put thread for receiving all messages here??
+
+            sthread.join();
+       },
+       None => println!("Could't connect to host"),
+    }
 }
